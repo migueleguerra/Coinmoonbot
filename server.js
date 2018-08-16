f4cafcf1aea2abfd462634bdd585c4e6cc85ed66
Miguel Enrique Guerra Topete <guetop@MacBook-Pro-de-Miguel.local>
2018-08-16 12:11:03 -0500

    procfile

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-'use strict';
-const Telegram = require('telegram-node-bot');
 const express = require('express');
 const packageInfo = require('./package.json');
-const keys = require('./config/keys');
 
 const app = express();
 
@@ -10,30 +7,7 @@ app.get('/', function(req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var PORT;
-if (process.env.NODE_ENV === 'production') PORT = process.env.PORT;
-
-const tg = new Telegram.Telegram(keys.telegram_key, {
-  workers: 1,
-  webAdmin: {
-    port: PORT,
-    host: 'localhost'
-  }
-});
-
-const InfoController = require('./controllers/info');
-const StartController = require('./controllers/start');
-const OtherwiseController = require('./controllers/other');
-const infoCtrl = new InfoController();
-
-tg.router
-  .when(new Telegram.TextCommand('/cm', 'cmCommand'), infoCtrl)
-  .when(new Telegram.TextCommand('/convert', 'convertCommand'), infoCtrl)
-  .when(new Telegram.TextCommand('/fcm', 'fcmCommand'), infoCtrl)
-  .when(
-    new Telegram.TextCommand('/start', 'startCommand'),
-    new StartController()
-  )
-  .otherwise(new OtherwiseController());
+// var PORT;
+// if (process.env.NODE_ENV === 'production') PORT = process.env.PORT;
 
 app.listen(process.env.PORT);
